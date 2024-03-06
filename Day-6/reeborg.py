39edf5f323679e118081171ee2731bede0a48b5b
cgozde <cemreuyar0@gmail.com>
2024-03-06 16:43:05 +0300

    commit

---
+++ Day-6/reeborg.py
@@ -0,0 +1,18 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+        if front_is_clear():
+            move()
+    elif wall_on_right():
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+                
+    
